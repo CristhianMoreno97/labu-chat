b3eb5dff704de8d0dc62fb685fd05b4294fba494
fernandom <cristian_21_97@hotmail.com>
2023-05-19 00:12:40 -0500

    basic test

---
+++ src/lib/productCard.svelte
@@ -0,0 +1,10 @@
+
+<div class="card">
+    <header class="card-header"><h2>Product Name</h2></header>
+    <div class="card-body px-4">Price: $10</div>
+    <div class="card-body px-4">Quantity: <strong>2</strong></div>
+    <div class="card-footer">
+        <button class="btn variant-glass-primary">Add</button>
+        <button class="btn variant-glass-error">Add</button>
+    </div>
+</div>
