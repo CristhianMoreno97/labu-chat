32af241c27d55f13ac3a29bc4f43a27f6f0d9318
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-22 17:46:44 -0500

    Refactorizar el código para mejorar la legibilidad y mantenibilidad.
    Se extrajeron funciones separadas para enviar mensajes de usuario y manejar respuestas del chatbot.
    
    Se implementó validación de entrada para asegurarse de que el mensaje del usuario cumpla con los criterios requeridos.
    
    Se mejoró el manejo de errores proporcionando retroalimentación o notificaciones al usuario.
    
    Se utilizó la destructuración de objetos para crear nuevos objetos de mensaje.
    
    Se actualizó la función handleKeyDown para utilizar las nuevas funciones para enviar mensajes y manejar respuestas.

--- src/lib/chatInput.svelte
+++ src/lib/chatInput.svelte
@@ -2,15 +2,57 @@
 
 
 <script lang="ts">
-	import {chatRoomMessages, type Message} from '../store';
-	import {postChatbotRequest} from '$lib/api/chatbot';
+	import { chatRoomMessages, type Message } from '../store';
+	import { postChatbotRequest } from '$lib/api/chatbot';
 
 	/**
-	 * Handles the keydown event when a user presses a key.
-	 * If the Enter key is pressed, a new message is added to the chat room content.
-	 * @param keyboardEvent The keyboard event object.
+	 * Envía un nuevo mensaje del usuario al chat.
+	 * @param userMessageContent El contenido del mensaje del usuario.
 	 */
-	async function handleKeyDown(keyboardEvent: KeyboardEvent): Promise<void> {		
+	function sendUserMessage(userMessageContent: string): void {
+		const newUserMessage: Message = {
+			id: 0,
+			host: true,
+			avatar: 48,
+			name: 'Jane',
+			timestamp: 'Yesterday @ 2:30pm',
+			content: userMessageContent,
+			color: 'variant-soft-primary',
+		};
+		chatRoomMessages.update((messages) => [...messages, newUserMessage]);
+	}
+
+	/**
+	 * Envía una solicitud al chatbot y agrega la respuesta al chat.
+	 * @param userMessageContent El contenido del mensaje del usuario.
+	 */
+	async function handleChatbotResponse(userMessageContent: string): Promise<void> {
+		try {
+			const response = await postChatbotRequest(userMessageContent);
+			const wizardMessageContent = response.message;
+			const newWizardMessage: Message = {
+				id: 1,
+				host: false,
+				avatar: 14,
+				name: 'Michael',
+				timestamp: 'Yesterday @ 2:45pm',
+				content: wizardMessageContent,
+				color: 'variant-soft-primary',
+			};
+			console.log(response);
+			chatRoomMessages.update((messages) => [...messages, newWizardMessage]);
+		} catch (error) {
+			console.log('Error:', error);
+			// TODO Mostrar mensaje de error al usuario o proporcionar notificación
+		}
+	}
+
+	/**
+	 * Maneja el evento keydown cuando el usuario presiona una tecla.
+	 * Si se presiona la tecla Enter, se agrega un nuevo mensaje al contenido del chat.
+	 * @param keyboardEvent El objeto de evento de teclado.
+	 */
+	async function handleKeyDown(keyboardEvent: KeyboardEvent): Promise<void> {	
 		if (keyboardEvent.key === 'Enter') {
 			const inputElement = keyboardEvent.target as HTMLInputElement; // util: get object name: console.log(event.target?.constructor.name);
 			const userMessageContent = inputElement?.value.trim(); 
@@ -18,34 +60,8 @@
 
 			if (userMessageContent === '') return;
 
-			const newUserMessage: Message = {
-				id: 0,
-				host: true,
-				avatar: 48,
-				name: 'Jane',
-				timestamp: 'Yesterday @ 2:30pm',
-				content: userMessageContent,
-				color: 'variant-soft-primary',
-			};
-			chatRoomMessages.update((messages) => [...messages, newUserMessage]);
-
-			try {
-				const response = await postChatbotRequest(userMessageContent);
-				const wizardMessageContent = response.message;
-				const newWizardMessage: Message = {
-					id: 1,
-					host: false,
-					avatar: 14,
-					name: 'Michael',
-					timestamp: 'Yesterday @ 2:45pm',
-					content: wizardMessageContent,
-					color: 'variant-soft-primary',
-				};
-				console.log(response);
-				chatRoomMessages.update((messages) => [...messages, newWizardMessage]);
-			} catch (error) {
-				console.log('Error:', error);
-			}
+			sendUserMessage(userMessageContent);
+			await handleChatbotResponse(userMessageContent);
 		}
 	}
 </script>
