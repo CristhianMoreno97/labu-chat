84b8a078f891e42cacaf81646fde2e2ca2efa9bb
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-22 19:11:36 -0500

    Ajustes visuales

--- src/lib/chat.svelte
+++ src/lib/chat.svelte
@@ -2,21 +2,29 @@
 
 
 <div id="columns" class="w-full grid grid-cols-[auto_1fr] gap-1 h-full">
-	<div id="left-rows" class="bg-surface-500/30 p-4">
+	<div id="left-rows" class="bg-secondary-100/90 p-4">
 		<div class="h-full grid grid-rows-[auto_1fr_auto] gap-1">
-			<div id="empty-section-1" class="bg-surface-500/30 p-4">(search)</div>
-			<div id="empty-section-2" class="bg-surface-500/30 p-4">(list)</div>
-			<div id="empty-section-3" class="bg-surface-500/30 p-4">(footer)</div>
+			<div id="empty-section-1" class="bg-section p-4"></div>
+			<div id="empty-section-2" class="bg-section p-4"></div>
+			<div id="empty-section-3" class="bg-section p-4"></div>
 		</div>
 	</div>
-	<div id="right-rows" class="bg-surface-500/30 p-4">
+	<div id="right-rows" class="bg-secondary-100/90 p-4">
 		<div class="h-full grid grid-rows-[1fr_auto] gap-1">
-			<div id="chat-content" class="bg-surface-500/30 p-4 overflow-y-auto">
+			<div id="chat-content" class="bg-section p-4 overflow-y-auto">
                 <slot name="chat-content">(chat content)</slot>
             </div>
-			<div id="chat-input" class="bg-surface-500/30 p-4">
+			<div id="chat-input" class="bg-section p-4">
 				<slot name="chat-input">(prompt)</slot>
 			</div>
 		</div>		
 	</div>
 </div>
+
+<style>
+
+	.bg-section {
+		background-color: var(--surface-500);
+	}
+
+</style>
