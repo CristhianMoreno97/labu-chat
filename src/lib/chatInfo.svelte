2c5c505f4172bffda3ae5821bf3239a52c9ded98
CristhianMoreno97 <cristian_21_97@hotmail.com>
2023-05-23 03:34:46 -0500

    Configurar enlaces externos

--- src/lib/chatInfo.svelte
+++ src/lib/chatInfo.svelte
@@ -1,24 +1,40 @@
 <div class="card bg-secondary-100/90">
 	<header class="card-header"><img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Logo_de_la_UPTC.svg" alt="uptc-logo"></header>
 	<section class="p-4">
-        <h1 class="text-2xl font-bold">Ramiro ChatBot</h1>
+        <h1 class="text-2xl font-bold mt-8">Ramiro ChatBot</h1>
         <h2 class="text-16px">¡Bienvenido al chatbot de servicios de laboratorios universitarios!</h2>    
         <p class="text-16px list mt-10">Participantes del proyecto: </p>
         <ul class="list mt-4 ml-4">
             <li>
-                <span><img class="rounded-full" src="https://avatars.githubusercontent.com/u/71224875?v=4" alt="avatar-cristhian-git" width="30px"></span>
+                <span>
+                    <a href="https://github.com/EmersonUrielVargas" target="_blank">
+                        <img class="rounded-full" src="https://avatars.githubusercontent.com/u/71224875?v=4" alt="avatar-emerson-git" width="30px">
+                    </a>
+                </span>
                 <span class="flex-auto">Emerson Uriel Vargas Alba </span>
             </li>
             <li>
-                <span><img class="rounded-full" src="https://avatars.githubusercontent.com/u/60074921?v=4" alt="avatar-cristhian-git" width="30px"></span>
+                <span>
+                    <a href="https://github.com/Frankma0117" target="_blank">
+                        <img class="rounded-full" src="https://avatars.githubusercontent.com/u/60074921?v=4" alt="avatar-frankin-git" width="30px">
+                    </a>
+                </span>
                 <span class="flex-auto">Frankin Manuel Cañón Gualteros </span>
             </li>
             <li>
-                <span><img class="rounded-full" src="https://avatars.githubusercontent.com/u/92770549?s=48&v=4" alt="avatar-cristhian-git" width="30px"></span>
+                <span>
+                    <a href="https://github.com/CristhianMoreno97" target="_blank">
+                        <img class="rounded-full" src="https://avatars.githubusercontent.com/u/92770549?s=48&v=4" alt="avatar-cristhian-git" width="30px">
+                    </a>
+                </span>
                 <span class="flex-auto">Cristhian Fernando Moreno Manrique</span>
             </li>
             <li>
-                <span><img class="rounded-full" src="https://avatars.githubusercontent.com/u/124376524?v=4" alt="avatar-cristhian-git" width="30px"></span>
+                <span>
+                    <a href="https://github.com/WCASTA2" target="_blank">
+                        <img class="rounded-full" src="https://avatars.githubusercontent.com/u/124376524?v=4" alt="avatar-william-git" width="30px">
+                    </a>
+                </span>
                 <span class="flex-auto"> William Julián Castañeda Ruiz</span>
             </li>
             <!-- ... -->
@@ -27,12 +43,12 @@
 	<footer class="card-footer mt-20">
         <ul class="list">
             <div class="logo-cloud grid-cols-1 lg:!grid-cols-2 gap-1">
-                <a class="logo-item" href="https://github.com/CristhianMoreno97/labu-chat/">
-                    <span><img class="rounded-full" src="https://cdn-icons-png.flaticon.com/512/3829/3829193.png" alt="avatar-cristhian-git" width="30px"></span>
+                <a class="logo-item" href="https://github.com/CristhianMoreno97/labu-chat/" target="_blank">
+                    <span><img class="rounded-full" src="https://cdn-icons-png.flaticon.com/512/3829/3829193.png" alt="frontend-logo" width="30px"></span>
                     <span>Frontend repository</span>
                 </a>
-                <a class="logo-item" href="https://github.com/EmersonUrielVargas/chatbot_lab_backend">
-                    <span><img class="rounded-full" src="https://cdn-icons-png.flaticon.com/512/2282/2282215.png " alt="avatar-cristhian-git" width="30px"></span>
+                <a class="logo-item" href="https://github.com/EmersonUrielVargas/chatbot_lab_backend" target="_blank">
+                    <span><img class="rounded-full" src="https://cdn-icons-png.flaticon.com/512/2282/2282215.png " alt="backend-logo" width="30px"></span>
                     <span>Backend repository</span>
                 </a>
                 <!-- ... -->
